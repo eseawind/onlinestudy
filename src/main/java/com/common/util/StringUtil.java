15664b65726ebe7bef2f0792c81ba22dc2e0698a
Mortimer <Mortimer@Mortimer-PC>
2015-09-13 12:53:22 +0800

    添加日志

--- src/main/java/com/common/util/StringUtil.java
+++ src/main/java/com/common/util/StringUtil.java
@@ -3,9 +3,16 @@ package com.common.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+--------------------------个人声明-----------------------------
+* @Project: onlinestudy
+* @File: StringUtil.java
+* @Date: 2015年9月12日 下午5:32:56
+* @Author: ZHUANGZHIXUAN 
+*/
 public class StringUtil {
 	
-	private final Logger logger = LoggerFactory.getLogger(StringUtil.class);
+	private final static Logger logger = LoggerFactory.getLogger(StringUtil.class);
 	
 	/**
 	 * 首字母大写
@@ -13,6 +20,7 @@ public class StringUtil {
 	 * @return 返回新的字符串，首字母大写
 	 */
 	public static String capitalize(String str){
+		logger.debug("返回首字母大写的字符串");
 		//参数判断
 		if("".equals(str) || str == null)
 			return "";
