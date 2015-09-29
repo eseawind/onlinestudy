245a63c132082019464958c3eef708eea696233e
Mortimer <zzx1994@qq.com>
2015-09-29 14:28:15 +0800

    测试Manager

--- src/main/java/com/xg12/web/TestController.java
+++ src/main/java/com/xg12/web/TestController.java
@@ -5,8 +5,11 @@ import javax.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.xg12.entity.User;
 
 /**
 --------------------------个人声明-----------------------------
@@ -18,6 +21,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class TestController extends AbstractController{
 	
+	/** 日志 */
 	private static Logger logger = LoggerFactory.getLogger(TestController.class);
 
 	@RequestMapping("test")
@@ -32,6 +36,14 @@ public class TestController extends AbstractController{
 		logger.debug("进入测试bootstrap页面");
 		logger.debug("===============================");
 		return "test/bootstrap";
-		
+	}
+	
+	@RequestMapping("test/save/user")
+	public void testSaveUser(ModelMap modelMap){
+		User user = userManager.selectByPrimaryKey(1);
+		user.setPassword("lalala");
+		userManager.updateByPrimaryKey(user);
+		logger.info("userDao :{}", userManager);
+		logger.info("test save user");
 	}
 }
