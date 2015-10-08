f5cc239f54b0b2a5448d4ebf4626e565b7471fdb
Mortimer <zzx1994@qq.com>
2015-10-08 17:02:28 +0800

    登录拦截器，只有登录的用户才能够使用系统

--- src/main/java/com/xg12/interceptor/LoginInterceptor.java
+++ src/main/java/com/xg12/interceptor/LoginInterceptor.java
@@ -1,5 +1,7 @@
 package com.xg12.interceptor;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -10,6 +12,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.common.util.Constants;
+import com.common.util.WebUtil;
 
 /**
  * 登录拦截器
@@ -23,6 +26,8 @@ public class LoginInterceptor implements HandlerInterceptor{
 	
 	/** 日志 */
 	private final Logger logger = LoggerFactory.getLogger(getClass());
+	
+	private List<String> excludeUrls;
 
 	public void afterCompletion(HttpServletRequest request,
 			HttpServletResponse response, Object object, Exception e)
@@ -37,17 +42,40 @@ public class LoginInterceptor implements HandlerInterceptor{
 
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
 			Object object) throws Exception {
+		logger.debug("excludeUrls:{}", excludeUrls);
 		// 判断session是否有user对象
 		HttpSession httpSession = request.getSession();
 		// TODO 对于不进行拦截的链接需要采取另外的方式
-		if(!"/onlinestudy/login".equals(request.getRequestURI()) && 
+		if(!isExclude(request) && 
 				httpSession.getAttribute(Constants.SESSION_USER) == null){
-			logger.debug("需要先登录,ip：{}",request.getRequestURI(), request.getRemoteAddr());
-			response.sendRedirect("login");
+			logger.debug("需要先登录,ip：{}", request.getRemoteAddr());
+			// 跳转到项目的路径 + 登录路径
+			response.sendRedirect(request.getContextPath() + "/login");
 			return false;
 		};
 		return true;
 	}
+	
+	/**
+	 * 当前请求路径是否在排除的链接之内
+	 * @param request
+	 * @return
+	 */
+	public boolean isExclude(HttpServletRequest request){
+		// 遍历需要排除的链接
+		for(String uri : excludeUrls){
+			if(uri.equals(WebUtil.getURI(request))){
+				return true;
+			}
+		}
+		return false;
+	}
 
+	public List<String> getExcludeUrls() {
+		return excludeUrls;
+	}
 
+	public void setExcludeUrls(List<String> excludeUrls) {
+		this.excludeUrls = excludeUrls;
+	}
 }
